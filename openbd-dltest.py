cda6198c698c8c5d4fb7ea714dc1c3b29de994ec
qdkmk <qdokuhon@gmail.com>
2019-10-08 16:08:06 +0900

    first commit

---
+++ openbd-dltest.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+openBDから公開されているすべての情報を取得するサンプル
+テスト環境 : Python3.7.1
+依存モジュール : requests (pip install requests)
+"""
+
+import multiprocessing
+import time
+import requests
+import json
+
+OPENBD_ENDPOINT = 'https://api.openbd.jp/v1/'
+
+
+def chunked(iterable, n):
+    """
+    リストをn個単位のリストに分割する
+    http://cortyuming.hateblo.jp/entry/2015/12/26/091224
+    """
+    return [iterable[x:x + n] for x in range(0, len(iterable), n)]
+
+
+def get_coverage():
+    """
+    openBDから収録ISBNの一覧を取得
+    :return: ISBNのリスト
+    """
+    return requests.get(OPENBD_ENDPOINT + 'coverage').json()
+
+
+def get_bibs(items):
+    """
+    openBDからPOSTでデータを取得する
+    :param items: ISBNのリスト
+    :return: 書誌のリスト
+    """
+    return requests.post(OPENBD_ENDPOINT + 'get', data={'isbn': ','.join(items)}).json()
+
+def get_bib(item):
+    """
+    openBDからPOSTでデータを取得する
+    :param items: ISBNのリスト
+    :return: 書誌のリスト
+    """
+    print(requests.post(OPENBD_ENDPOINT + 'get', data={'isbn': ','.join(item)}).json())
+    return requests.post(OPENBD_ENDPOINT + 'get', data={'isbn': ','.join(item)}).json()
+
+if __name__ == '__main__':
+
+    # ISBNのリストを10000件単位に分割
+    #chunked_coverage = chunked(get_coverage(), 10000)
+    result = get_bib("9784766108958")
+    #print(result)
+    print(json.dumps(result, indent=4))
+
+    headers = {"content-type": "application/json"}
+    url="https://api.openbd.jp/v1/get?isbn=9784766108958"
+    r = requests.get(url, headers=headers)
+    data = r.json()
+    print(data[0]["summary"]["title"])
+    # 4プロセスの並列でダウンロード
+    # マジックナンバー:openBDインフラ的に4接続が最適
+    #p = multiprocessing.Pool(4)
+    #results = p.imap_unordered(get_bibs, chunked_coverage)
+    #cnt = 0
+    #start = time.time()
+    #for result in results:
+    #    cnt += len(result)
+    #    print(cnt)
+    #    for bib in result:
+            # ここで書誌1件単位の処理　exp:インデックス化など
+    #        if bib and 'summary' in bib:
+                # Coverageにあっても、実データがない場合（Noneが返る）を想定すること
+                # （複数サーバーのデータ同期が遅れる場合があるため）
+    #            print(bib['summary']['isbn'])
+
+#    print(cnt,time.time()-start)
